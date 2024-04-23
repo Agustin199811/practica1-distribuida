9ca72b5ac3e364261d4e064f7d6032381a3e3ff8
Agustin <toapantaagustin9c@gmail.com>
2024-04-22 19:18:23 -0500

    My first commit whith spring boot

---
+++ src/main/java/com/uce/practica1distribuidas/Practica1DistribuidasApplication.java
@@ -0,0 +1,13 @@
+package com.uce.practica1distribuidas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica1DistribuidasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica1DistribuidasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uce/practica1distribuidas/Practica1DistribuidasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uce.practica1distribuidas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica1DistribuidasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
